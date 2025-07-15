acaaf3cd61fdee5710cc03ad57f45735173f38b0
scott christian mccallum <scottcmccallum@gmail.com>
2025-07-15 13:45:44 -0400

    docs folder

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,20 @@
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
         adapter: adapter({
-            // default options are shown. On some platforms
-            // these options are set automatically — see below
-            pages: 'build',
-            assets: 'build',
-            fallback: undefined,
+            pages: 'docs',      // Changed from 'build' to 'docs'
+            assets: 'docs',     // Changed from 'build' to 'docs'
+            fallback: null,
             precompress: false,
             strict: true
-        })
+        }),
+        paths: {
+            base: dev ? '' : process.env.BASE_PATH
+        }
     }
 };
 
