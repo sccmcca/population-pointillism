414b6338c0015bf1a0ef6274d070a209c1c43f86
scott christian mccallum <scottcmccallum@gmail.com>
2025-07-15 13:57:40 -0400

    rebuild

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,28 @@
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-auto';
+// import preprocess from 'svelte-preprocess';
+import adapter from "@sveltejs/adapter-static"; 
 
-const dev = process.argv.includes('dev');
+const dev = "production" === "development";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    kit: {
-        adapter: adapter({
-            pages: 'docs',
-            assets: 'docs',
-            fallback: null,
-            precompress: false,
-            strict: true
-        }),
-        paths: {
-            base: dev ? '' : '/population-pointillism'  // Updated to match your new repo name
-        }
-    }
+
+	// // note that previous projects have included this preprocess, but I don't think it's necessary, we'll see when build
+	// preprocess: preprocess(),
+
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter({
+		    pages: "docs",
+		    assets: "docs"
+		}),
+		paths: {
+		    // change below to your repo name
+		    base: dev ? "" : "/population-pointillism",
+		}
+	}
 };
 
 export default config;
